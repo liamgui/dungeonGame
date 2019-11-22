7c3f7fe6eb470d097b71988b7556dc714d235b11
Will Guidinger <guidkid3@gmail.com>
2019-11-22 08:42:28 -0500

    index.php?

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<title>Game</title>
+		<link rel="stylesheet" type="text/css" href="styles.css" />
+	</head>
+	<body>
+		<canvas id="background"></canvas>
+		<!--    <canvas id="interface"></canvas> -->
+		<canvas id="game"></canvas>
+		
+		<p style="margin-top:500px;">hello</p>
+		<!-- <script src="background.js"></script> -->
+		<script src="main.js" type="module"></script>
+	</body>
+</html>
+
