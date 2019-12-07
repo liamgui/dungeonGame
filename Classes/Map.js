6f780810dfb0e93ea16953517cda76c5ca02fe7e
Will Guidinger <guidkid3@gmail.com>
2019-12-06 22:08:13 -0500

    generator work

--- Classes/Map.js
+++ Classes/Map.js
@@ -6,20 +6,28 @@ export default {
 
 		initialMap = this.createChunk(Global.chunkSize, initialMap, [0,0]);
 		// console.log(initialMap);
-		initialMap = this.chunkPerimeterCheck(0, initialMap, true);
+		initialMap = this.chunkPerimeterCheck(initialMap, true);
 		
 		return initialMap;
 	},
+	createMap: function() {
+		let map = {
+			chunkGrid: [[]],
+			chunkList: {}
+		};
+		return map;
+	},
+
 	//need to pass current Position of chunk in chunkGrid?
-	chunkPerimeterCheck: function(currentChunkId, map, add = false) {
+	chunkPerimeterCheck: function(map, check = false, thisChunk = this.getCurrentChunk()) {
 		let directions = ['n','e','s','w'];
 		let nextPosition;
 		let positions = [];
-		if (add === true) {
+		if (!check) {
 			directions.forEach((direction, index) => {
-				nextPosition = this.chunkDirectionToPosition(direction);
+				nextPosition = this.chunkDirectionToPosition(direction, thisChunk);
 				console.log(nextPosition);
-				if (!this.checkForChunk(nextPosition, map)) {
+				if (!this.checkForChunk(nextPosition, map, thisChunk)) {
 					// console.log("Perimeter Chunk Needed");
 					map = this.createChunk(Global.chunkSize, map, nextPosition);
 				}
@@ -28,7 +36,7 @@ export default {
 			
 		} else {
 			directions.forEach((direction, index) => {
-				nextPosition = this.chunkDirectionToPosition(direction);
+				nextPosition = this.chunkDirectionToPosition(direction, thisChunk);
 				console.log(nextPosition);
 				if (!this.checkForChunk(nextPosition, map)) {
 					// console.log("Perimeter Chunk Needed");
@@ -39,13 +47,13 @@ export default {
 			return positions;
 		}
 	},
-	checkForChunk: function(position, map) {
+	checkForChunk: function(position, map, thisChunk) {
 		if (
-			this.getCurrentChunk()[0] + position[0] < 0 ||
-			this.getCurrentChunk()[0] + position[0] > map.chunkGrid.length - 1 ||
-			this.getCurrentChunk()[1] + position[1] < 0 ||
-			this.getCurrentChunk()[1] + position[1] > map.chunkGrid[0].length - 1 ||
-			map.chunkGrid[this.getCurrentChunk()[0] + position[0]][this.getCurrentChunk()[1] + position[1]] === null
+			thisChunk[0] + position[0] < 0 ||
+			thisChunk[0] + position[0] > map.chunkGrid.length - 1 ||
+			thisChunk[1] + position[1] < 0 ||
+			thisChunk[1] + position[1] > map.chunkGrid[0].length - 1 ||
+			map.chunkGrid[thisChunk[0] + position[0]][thisChunk[1] + position[1]] === null
 		) {
 			// console.log(false);
 			return false
@@ -67,16 +75,16 @@ export default {
 		}
 		return position;
 	},
-	chunkDirectionToPosition: function(direction) {
+	chunkDirectionToPosition: function(direction, thisChunk) {
 		let position = [];
 		if (direction === 'n') {
-			position = [(this.getCurrentChunk()[0] + -1), (this.getCurrentChunk()[1] + 0)];
+			position = [(thisChunk[0] + -1), (thisChunk[1] + 0)];
 		} else if (direction === 'e') {
-			position = [(this.getCurrentChunk()[0] + 0), (this.getCurrentChunk()[1] + 1)];
+			position = [(thisChunk[0] + 0), (thisChunk[1] + 1)];
 		} else if (direction === 's') {
-			position = [this.getCurrentChunk()[0] + 1, this.getCurrentChunk()[1] + 0];
+			position = [thisChunk[0] + 1, thisChunk[1] + 0];
 		} else if (direction === 'w') {
-			position = [this.getCurrentChunk()[0] + 0, this.getCurrentChunk()[1] + -1];
+			position = [thisChunk[0] + 0, thisChunk[1] + -1];
 		}
 		return position;
 	},
@@ -103,110 +111,6 @@ export default {
 
 	generateChunk: function(dungeonChunk, map, gridPosition) {
 		let types = ["o", "w", "d"];
-		// for(let rowNum = 0; rowNum < dungeonChunk.length; rowNum++) {
-		// 	let row = dungeonChunk[rowNum];
-		// 	for(let spot = 0; spot < dungeonChunk[rowNum].length; spot++) {
-		// 		let tile = row[spot];
-		// 		let tileBuild = [];
-		// 		//Random generation based on seed
-		// 		for (let s = 0; s < 4; s++) {
-		// 			let rand = Math.floor(Math.seedRandom(0, 10));
-		// 			if (rand < 4) {
-		// 				tileBuild[s] = types[0];
-		// 			} else if (rand < 8) {
-		// 				tileBuild[s] = types[1];
-		// 			} else {
-		// 				tileBuild[s] = types[2];
-		// 			}
-		// 		}
-		// 		// End random generation
-				
-		// 		//Build the tileName
-		// 		tile.tileBuild = tileBuild;
-		// 		tileBuild = tileBuild.join("");
-		// 		tile.tileType = tileBuild;
-		// 	}
-		// }
-		
-		// //2nd pass
-		// for(let rowNum = 0; rowNum < dungeonChunk.length; rowNum++) {
-		// 	let row = dungeonChunk[rowNum];
-		// 	for(let spot = 0; spot < dungeonChunk[rowNum].length; spot++) {
-		// 		let tile = row[spot];
-		// 		if (rowNum > 0 && rowNum < dungeonChunk.length - 1 && spot > 0 && spot < rowNum.length - 1) {
-		// 			let north = dungeonChunk[rowNum - 1][spot];
-		// 			let northEast = dungeonChunk[rowNum - 1][spot + 1];
-		// 			let east = row[spot + 1];
-		// 			let southEast = dungeonChunk[rowNum + 1][spot + 1];
-		// 			let south = dungeonChunk[rowNum + 1][spot];
-		// 			let southWest = dungeonChunk[rowNum + 1][spot - 1];
-		// 			let west = row[spot - 1];
-		// 			let northWest = dungeonChunk[rowNum - 1][spot - 1];
-		// 		}
-				
-		// 		//Modify tiles based on weights of proximity?
-		// 		if (rowNum > 0) {
-		// 			tile.tileBuild[0] = dungeonChunk[rowNum - 1][spot].tileType[2];
-		// 		}
-		// 		if (spot > 0) {
-		// 			tile.tileBuild[3] = row[spot - 1].tileType[1];
-		// 		}
-		// 	}
-		// }
-		// //final pass
-		// for(let rowNum = 0; rowNum < dungeonChunk.length; rowNum++) {
-		// 	let row = dungeonChunk[rowNum];
-		// 	for(let spot = 0; spot < dungeonChunk[rowNum].length; spot++) {
-		// 		let tile = row[spot];
-		// 		if (tile.tileBuild === ["w", "w", "w", "w"]) {
-		// 			let rand = Math.seedRandom(0, 10);
-		// 			if (rand < 4) {
-		// 				rand = Math.seedRandom(0, 3);
-		// 				tile.tileBuild[rand] = "o";
-		// 			} else if (rand < 6) {
-		// 				rand = Math.seedRandom(0, 3);
-		// 				tile.tileBuild[rand] = "d";
-		// 			} else if (rand < 8) {
-		// 				tile.solid = true;
-		// 			}
-		// 		}
-
-		// 		// Begin modifying dungeon to match previous tiles
-		// 		if (rowNum > 0) {
-		// 			tile.tileBuild[0] = dungeonChunk[rowNum - 1][spot].tileType[2];
-		// 			// console.log(gridPosition);
-		// 			this.checkForChunk(gridPosition,map);
-		// 		}
-
-		// 		if (spot > 0) {
-		// 			tile.tileBuild[3] = row[spot - 1].tileType[1];
-		// 		}
-		// 		// console.log(this.getCurrentChunk());
-		// 		// console.log(map);
-		// 		// check for neighboring chunks and adjust tile
-		// 		// if(Global.currentChunk !== [0,0]) {
-		// 			if (rowNum == 0 && this.checkForChunk([gridPosition[0] - 1, gridPosition[1]], map)) {
-		// 				// let northOfChunk = map.chunkGrid[gridPosition[0]][gridPosition[1]];
-		// 				// console.log("There is a chunk above", gridPosition);
-		// 				// tile.tileBuild[0] = northOfChunk[Global.chunkSize - 1][];
-		// 			}
-		// 			if (rowNum == Global.chunkSize - 1 && this.checkForChunk([gridPosition[0] + 1, gridPosition[1]], map)) {
-		// 				// console.log("There is a chunk below", gridPosition);
-
-		// 			}
-		// 			if (spot == 0 && this.checkForChunk([gridPosition[0], gridPosition[1] - 1], map)) {
-		// 				// console.log("There is a chunk left", gridPosition);
-		// 			}
-
-		// 			if (spot == Global.chunkSize - 1 && this.checkForChunk([gridPosition[0], gridPosition[1] + 1], map)) {
-		// 				// console.log("There is a chunk right", gridPosition);
-		// 			}
-		// 		// }					
-		// 		// End modifying dungeon to match previous tiles
-
-		// 		tile.tileType = tile.tileBuild.join("");
-		// 		}
-		// 	}
 		//first pass		
 		dungeonChunk.forEach((row, rowNum) => {
 				row.forEach((tile, spot) => {
@@ -279,26 +183,28 @@ export default {
 				if (spot > 0) {
 					tile.tileBuild[3] = row[spot - 1].tileType[1];
 				}
-
+				let perimeterChunks = this.chunkPerimeterCheck(map, true, gridPosition);
+				//! rewrite positions array in chunkPerimeterCheck to default to false if not available
+				//! set chunks below to positions (chunkGrid or relative positions?)
 				let northChunk = false;
 				let eastChunk = false;
 				let southChunk = false;
 				let westChunk = false;
+
 				// if(Global.currentChunk !== [0,0]) {
-					if (rowNum == 0 && northChunk) {
+					if (rowNum == 0 && northChunk !== false) {
 						// let northOfChunk = map.chunkGrid[gridPosition[0]][gridPosition[1]];
 						// console.log("There is a chunk above", gridPosition);
 						// tile.tileBuild[0] = northOfChunk[Global.chunkSize - 1][];
 					}
-					if (rowNum == Global.chunkSize - 1 && southChunk) {
+					if (rowNum == Global.chunkSize - 1 && southChunk !== false) {
 						// console.log("There is a chunk below", gridPosition);
-						
 					}
-					if (spot == 0 && westChunk) {
+					if (spot == 0 && westChunk !== false) {
 						// console.log("There is a chunk left", gridPosition);
 					}
 					
-					if (spot == Global.chunkSize - 1 && eastChunk) {
+					if (spot == Global.chunkSize - 1 && eastChunk !== false) {
 						// console.log("There is a chunk right", gridPosition);
 					}
 				// }					
@@ -309,14 +215,6 @@ export default {
 		});
 
 		return dungeonChunk;
-	},
-
-	createMap: function() {
-		let map = {
-			chunkGrid: [[]],
-			chunkList: {}
-		};
-		return map;
 	},
 
 	addChunk: function(map, chunk, position) {
