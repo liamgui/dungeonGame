bb2991226f5d954a6f6cb9ab6075efbcfda3a8af
Will Guidinger <guidkid3@gmail.com>
2019-12-13 14:14:55 -0500

    changed Math.seed to Global.seed

--- functions.js
+++ functions.js
@@ -1,21 +1,22 @@
 Math.seedRandom = function(max, min) {
-	if (Math.seed === undefined /* | typeof Math.seed === 'string' */) {
-		Math.seed = Math.floor(Math.random() * 1000);
+	
+	if (Global.seed === undefined /* | typeof Global.seed === 'string' */) {
+		Global.seed = Math.floor(Math.random() * 1000);
 	}
 	
-	if (typeof Math.seed === 'string') {
+	if (typeof Global.seed === 'string') {
 		let seed = 1; 
-		for (let c in Math.seed){
-			seed = seed * Math.seed.charCodeAt(c);
+		for (let c in Global.seed){
+			seed = seed * Global.seed.charCodeAt(c);
 		};
-		Math.seed = seed;
+		Global.seed = seed;
 	}
 	max = max || 1;
 	min = min || 0;
-	Math.seed = (Math.seed * 9301 + 49297) % 233280;
+	Global.seed = (Global.seed * 9301 + 49297) % 233280;
 	// console.log(Math.seed);
 	// console.log(Math.seed);
-	var rnd = Math.seed / 233280;
+	var rnd = Global.seed / 233280;
 	return min + rnd * (max - min);
 };
 
