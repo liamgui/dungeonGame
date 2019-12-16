ae52530c6edc69e233785900b526678a6371f632
Will Guidinger <guidkid3@gmail.com>
2019-12-15 22:23:59 -0500

    cleanup of explored/discovered

--- View.js
+++ View.js
@@ -163,11 +163,11 @@ export default {
 					row.forEach((tile, y) => {
 						tileHeight = (tile.tileHeight) * Global.zoomLevel;
 						tileWidth = tile.tileWidth * Global.zoomLevel;
-						if (tile.explored === true || Global.revealAll) {
+						if (tile.explored || Global.revealAll) {
 							ctx.fillStyle = "#181818";
 							ctx.fillRect(dx, dy, tileWidth - 0.15, tileHeight - 0.15);
 							ctx.drawImage(tileSets[tile.tileType], dx - 0.5, dy - 0.5, tileWidth, tileHeight );
-						} else if (tile.discovered === true) {
+						} else if (tile.discovered) {
 							ctx.fillStyle = "#383838";
 							ctx.fillRect(dx, dy, tileWidth - 0.15, tile.height - 0.15);
 							ctx.drawImage(tileSets[tile.tileType], dx - 0.5, dy - 0.5, tileWidth, tileHeight );
